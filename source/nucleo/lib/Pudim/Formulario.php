f05ca2e28bf3bed31491f65b3e5e0bfa5f6120cd
Francisco Ernesto Teixeira <fco.ernesto@gmail.com>
2015-05-10 23:02:24 -0300

    Corrige importação de uma biblioteca

--- source/nucleo/lib/Pudim/Formulario.php
+++ source/nucleo/lib/Pudim/Formulario.php
@@ -289,7 +289,7 @@ class Formulario
      */
     public static function validarCpf($cpf)
     {
-        require_once(__DIR__ . '/../../library/ValidarChaveFiscal.php');
+        require_once(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', '..', '..', 'library', 'ValidarChaveFiscal.php']));
 
         $validador = new \ValidarChaveFiscal($cpf, 'cpf');
 
